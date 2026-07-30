81606f4c6405fec4c3ed4ed1fe82ba65e3ee5fb4
ivinercassio <ivinercassio@gmail.com>
2026-07-30 16:00:04 -0300

    add getAllReportsByCommentId and getAllReportsByPostId

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Report
 from .serializers import ReportSerializer
 
@@ -9,3 +11,23 @@ class ReportViewSet(viewsets.ModelViewSet):
     queryset = Report.objects.all().order_by('-date_report')
     serializer_class = ReportSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    @action(detail=True, methods=['get'], url_path='post')
+    def by_post(self, request, pk=None):
+        """
+        GET /api/reports/:id/post
+        Retorna todas as denúncias associadas ao Post (:id)
+        """
+        post_reports = Report.objects.filter(post_id=pk).order_by('-date_report')
+        serializer = self.get_serializer(post_reports, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'], url_path='comment')
+    def by_comment(self, request, pk=None):
+        """
+        GET /api/reports/:id/comment
+        Retorna todas as denúncias associadas ao Comentário (:id)
+        """
+        comment_reports = Report.objects.filter(comment_id=pk).order_by('-date_report')
+        serializer = self.get_serializer(comment_reports, many=True)
+        return Response(serializer.data)
