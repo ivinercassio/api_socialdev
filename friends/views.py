43deb1bd5f8930b6bbd0cf8210acb73c826e1157
ivinercassio <ivinercassio@gmail.com>
2026-07-30 15:47:51 -0300

    add getAllFriendsByUserId

--- friends/views.py
+++ friends/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Q
 from .models import Friend
 from .serializers import FriendSerializer
 
@@ -13,3 +16,15 @@ class FriendViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # Associa automaticamente friend_one ao usuário autenticado
         serializer.save(friend_one=self.request.user)
+    
+    @action(detail=True, methods=['get'], url_path='user')
+    def by_user(self, request, pk=None):
+        """
+        GET /api/friends/:id/user
+        Retorna todas as relações de amizade do Usuário cujo ID é o parâmetro :id
+        """
+        user_friends = Friend.objects.filter(
+            Q(friend_one_id=pk) | Q(friend_two_id=pk)
+        )
+        serializer = self.get_serializer(user_friends, many=True)
+        return Response(serializer.data)
