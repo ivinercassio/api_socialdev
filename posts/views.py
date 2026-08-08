a7d0ad1403617ce3443cd8c79613a09c4fe1a96d
ivinercassio <ivinercassio@gmail.com>
2026-08-08 09:22:23 -0300

    add endpoint to get all friends posts

--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,10 @@ from rest_framework import viewsets, permissions, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.db.models import F
+from django.db.models import F, Q
 
 from .models import Post
+from friends.models import Friend
 from .serializers import PostSerializer
 from .permissions import IsAuthenticatedForWriteOrReadOnly
 
@@ -55,3 +56,27 @@ class PostViewSet(viewsets.ModelViewSet):
             post.save(update_fields=['like'])
             post.refresh_from_db()
         return Response(self.get_serializer(post).data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'], url_path='friends', permission_classes=[permissions.IsAuthenticated])
+    def friends_posts(self, request):
+        """
+        GET /api/posts/friends/
+        Retorna posts apenas de usuários com os quais o usuário logado possui amizade.
+        """
+        current_user = request.user
+
+        friendships = Friend.objects.filter(
+            Q(friend_one=current_user) | Q(friend_two=current_user)
+        )
+
+        friend_ids = set()
+        for friendship in friendships:
+            if friendship.friend_one_id == current_user.id:
+                friend_ids.add(friendship.friend_two_id)
+            else:
+                friend_ids.add(friendship.friend_one_id)
+
+        posts = Post.objects.filter(author_id__in=friend_ids).order_by('-date_published')
+
+        serializer = self.get_serializer(posts, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
